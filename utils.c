a58e57d99c09505073d763acd3da18016f7f97a6
nimiabaga <nimiabaga@yahoo.com>
2023-09-19 13:21:21 +0100

    printf update

--- utils.c
+++ utils.c
@@ -3,9 +3,9 @@
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
+ *
  * Return: 1 if c is printable, 0 otherwise
  */
-
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
@@ -15,17 +15,16 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascii in jexadecimal code to buffer
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
- * @ascii_code: ASCII CODE.
+ * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format codr is alwaus 2 digits long */
+	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
@@ -40,10 +39,10 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 /**
  * is_digit - Verifies if a char is a digit
- * @c: Chat to be evaluated
+ * @c: Char to be evaluated
+ *
  * Return: 1 if c is a digit, 0 otherwise
  */
-
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
@@ -60,7 +59,6 @@ int is_digit(char c)
  * Return: Casted value of num
  */
 long int convert_size_number(long int num, int size)
-
 {
 	if (size == S_LONG)
 		return (num);
@@ -77,13 +75,12 @@ long int convert_size_number(long int num, int size)
  *
  * Return: Casted value of num
  */
-
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
 	else if (size == S_SHORT)
 		return ((unsigned short)num);
+
 	return ((unsigned int)num);
 }
-
